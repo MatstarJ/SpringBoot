1d01874b7326ca775c06cb3cad983ac65330df91
MatStarJ <kibe1121@naver.com>
2021-12-13 01:35:45 +0900

    타임리프 연습

---
+++ SpringBootBoardEx3/src/main/java/com/matstar/ex03/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.matstar.ex03;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootBoardEx3Application.class);
+    }
+
+}
---
+++ SpringBootBoardEx3/src/test/java/com/matstar/ex03/SpringBootBoardEx3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matstar.ex03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardEx3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
